849e32cc2b9c00229d8726f5c47938dd9f9379b0
신동현 <donghyeun02@gmail.com>
2023-06-27 17:35:03 +0900

    MODIFY : .env.sample 수정 및 서버가 실행될 때 데이터베이스 초기화되도록 수정

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-require("dotenv").config();
+// require("dotenv").config();
 
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 const { DataSource } = require("typeorm");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
 
@@ -12,26 +14,26 @@ app.use(logger("combined"));
 app.use(express.json());
 
 const appDataSource = new DataSource({
-    type: process.env.DB_CONNECTION,
-    port: process.env.DB_PORT,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+  type: process.env.DB_CONNECTION,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
-app.listen(3000, function () {
-    console.log("server listening on port 3000");
-});
-
-appDataSource
+app.listen(3000, async () => {
+  console.log("server listening on port 3000");
+  await appDataSource
     .initialize()
     .then(() => {
-        console.log("Data Source has been initialized!");
+      console.log("Data Source has been initialized!");
     })
     .catch((err) => {
-        console.error("Error during Data Source initialization:", err);
+      console.error("Error during Data Source initialization:", err);
     });
+});
 
 app.get("/ping", (req, res) => {
-    res.json({ message: "pong" });
+  res.json({ message: "pong" });
 });
