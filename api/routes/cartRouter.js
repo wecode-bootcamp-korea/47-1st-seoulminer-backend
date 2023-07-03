aee9691d3c1f7c31ec61e1f73d299322f73dc989
lchoe24 <lena.choe02@gmail.com>
2023-07-03 10:50:12 +0900

    Modify: Cart function

--- api/routes/cartRouter.js
+++ api/routes/cartRouter.js
@@ -4,6 +4,6 @@ const { cartController } = require("../controllers")
 const router = express.Router();
 
 router.post('', cartController.createCartItem);
-router.patch('', cartController.createCartItem);
+router.patch('', cartController.updateCartItem);
 
 module.exports = router;
