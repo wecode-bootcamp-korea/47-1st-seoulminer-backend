60f009c77908cd14301121d8a039b70c3e47b7a0
lchoe24 <lena.choe02@gmail.com>
2023-07-05 23:15:27 +0900

    Modify: sql to js variable name

--- api/models/orderDao.js
+++ api/models/orderDao.js
@@ -3,10 +3,10 @@ const { appDataSource } = require("./dataSource");
 const orderItems = async (userId, orderNumber) => {
   try {
     return await appDataSource.query(
-      `SELECT orders.order_number as orderNumber, 
-              order_items.product_id, 
-              order_items.product_option_id, 
-              order_items.quantity 
+      `SELECT order_number as orderNumber, 
+              product_id as productId, 
+              product_option_id as productOptionId, 
+              quantity
       FROM order_items
       JOIN orders on orders.id = order_id
       WHERE user_id = ? AND order_number = ?
