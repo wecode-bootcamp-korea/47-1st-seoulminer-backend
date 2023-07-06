c774c7ead07b913459809939944661d4cd3f896c
신동현 <donghyeun02@gmail.com>
2023-07-06 18:37:05 +0900

    메소드 수정

--- api/routes/orderRouter.js
+++ api/routes/orderRouter.js
@@ -5,7 +5,7 @@ const { loginRequired } = require("../utils/auth");
 const router = express.Router();
 
 router.get("/:orderNumber", loginRequired, orderController.getOrderItems);
-router.get("/item", loginRequired, orderController.createUserOrderByItem);
+router.post("/item", loginRequired, orderController.createUserOrderByItem);
 router.post("/cart", loginRequired, orderController.createUserOrderByCart);
 
 module.exports = router;
